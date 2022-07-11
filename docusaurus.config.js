75fe950b5a79e27c2c2fa6ea51f59b32727df2b5
lvxw <lv81567395@vip.qq.com>
2022-07-11 23:39:04 +0800

    更新一下标题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'LXW---博客分享',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Day Day up!!!!',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
